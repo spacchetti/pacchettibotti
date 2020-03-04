479c59ca8b20f181fe126733074bc48845559ebc
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2020-03-04 13:32:35 +0100

    Add a nix-shell environment

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  easy-ps = import (
+    pkgs.fetchFromGitHub {
+      owner = "justinwoo";
+      repo = "easy-purescript-nix";
+      rev = "340e82b6ecaccc4059740e69f8ec18546b527481";
+      sha256 = "1q2ciwd3193kig1paidzrgxl60y4rb39bsi97lk7m6ff8mis6z6i";
+    }
+  ) {
+    inherit pkgs;
+  };
+
+in
+pkgs.runCommand "easy-ps-test" {
+  buildInputs = builtins.attrValues {
+    inherit (easy-ps) purescript psc-package dhall-simple dhall-json-simple spago;
+  } ++ [ pkgs.git pkgs.nodePackages.bower ];
+} ""
